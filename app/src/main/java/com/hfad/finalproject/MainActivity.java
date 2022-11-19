fd2a9c44a58c3fe27db2ac216f99a6ed4670997e
Austin <aw3335@desales.edu>
2022-11-19 14:48:52 -0500

    Austin - Main Menu Setup

--- app/src/main/java/com/hfad/finalproject/MainActivity.java
+++ app/src/main/java/com/hfad/finalproject/MainActivity.java
@@ -1,6 +1,17 @@
 package com.hfad.finalproject;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
-public class MainActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
